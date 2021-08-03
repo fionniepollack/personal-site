97d94c5c8e16833a1dfef630c02044eb7ce44d67
Fionnie Pollack <fionniepollack@gmail.com>
2021-08-03 14:48:43 -0400

    Add LinkedIn and Github links

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function About() {
-  return <div>THIS IS THE ABOUT PAGE</div>;
+  return  <div>THIS IS THE ABOUT PAGE
+            <ul>
+              <li><a href="https://www.linkedin.com/in/fionniepollack">LinkedIn</a></li>
+              <li><a href="https://github.com/fionniepollack">Github</a></li>
+            </ul>
+          </div>;
 }
 
 export default About;
