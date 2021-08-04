7fa251ed0e0499b54c851bfa58a24b350304b5d6
Fionnie Pollack <fionniepollack@gmail.com>
2021-08-04 16:06:41 -0400

    Add projects to work page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function About() {
-  return  <div>THIS IS THE ABOUT PAGE
-            <ul>
-              <img src="https://media-exp1.licdn.com/dms/image/C4E03AQF-oOuXXFeb3Q/profile-displayphoto-shrink_200_200/0/1615912245832?e=1633564800&v=beta&t=Yq0T4-BzqcCXcvmZA5fVlh4gGIZ6C4KQSdzMUuaJBKc"></img>
-              <li><a href="https://www.linkedin.com/in/fionniepollack">LinkedIn</a></li>
-              <li><a href="https://github.com/fionniepollack">Github</a></li>
-            </ul>
-          </div>;
+  return <div>THIS IS THE ABOUT PAGE
+    <ul>
+      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQF-oOuXXFeb3Q/profile-displayphoto-shrink_200_200/0/1615912245832?e=1633564800&v=beta&t=Yq0T4-BzqcCXcvmZA5fVlh4gGIZ6C4KQSdzMUuaJBKc"></img>
+      <li><a href="https://www.linkedin.com/in/fionniepollack">LinkedIn</a></li>
+      <li><a href="https://github.com/fionniepollack">Github</a></li>
+    </ul>
+  </div>;
 }
 
 export default About;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import recipe_berry from '../images/recipe_berry.png';
+
 
 function Work() {
-  return <div>THIS IS THE WORK PAGE</div>;
+  return <div>THIS IS THE WORK PAGE
+
+    <Card class="text-center" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={recipe_berry} />
+      <Card.Body>
+        <Card.Title>Recipe Berry</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Cras justo odio</ListGroupItem>
+        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+  </div>;
 }
 
 export default Work;
